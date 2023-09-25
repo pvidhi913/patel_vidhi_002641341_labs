5b58b20a1b2b61acbb96f4b727546cd268dda253
pvidhi913 <144813006+pvidhi913@users.noreply.github.com>
2023-09-25 15:17:58 -0400

    Add files via upload

---
+++ Lab-2/src/model/VitalSigns.java
@@ -0,0 +1,54 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author vidhi
+ */
+public class VitalSigns {
+    
+    private double temperature;
+    private double pressure;
+    private int pulse;
+    private String date;
+
+    public double getTemperature() {
+        return temperature;
+    }
+
+    public void setTemperature(double temperature) {
+        this.temperature = temperature;
+    }
+
+    public double getPressure() {
+        return pressure;
+    }
+
+    public void setPressure(double pressure) {
+        this.pressure = pressure;
+    }
+
+    public int getPulse() {
+        return pulse;
+    }
+
+    public void setPulse(int pulse) {
+        this.pulse = pulse;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
+    
+    @Override
+    public String toString(){
+        return date;
+    }
+}
