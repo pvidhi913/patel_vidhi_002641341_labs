b2e6bc0aacd9562bdef0d73630a2575e15b4c8f5
pvidhi913 <144813006+pvidhi913@users.noreply.github.com>
2023-10-12 23:00:50 -0400

    Add files via upload

---
+++ Lab 3/src/Business/Account.java
@@ -0,0 +1,67 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Business;
+
+import java.util.Date;
+
+/**
+ *
+ * @author vidhi
+ */
+public class Account {
+ 
+        private String routingNumber;
+        private String acountNumber;
+        private String bankName;
+        private int balance;
+        private Date createdOn;
+
+    public Account() {
+        this.createdOn = new Date();
+        
+    }
+    
+    public String getRoutingNumber() {
+        return routingNumber;
+    }
+
+    public void setRoutingNumber(String routingNumber) {
+        this.routingNumber = routingNumber;
+    }
+
+    public String getAcountNumber() {
+        return acountNumber;
+    }
+
+    public void setAcountNumber(String acountNumber) {
+        this.acountNumber = acountNumber;
+    }
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+
+    public Date getCreatedOn() {
+        return createdOn;
+    }
+
+    public String toString(){
+        return bankName;
+      
+    }
+        
+}
